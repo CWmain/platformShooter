3763e3560d9fe7d548cb95d4211f7e7cd6a297b6
CWmain <cameronwhite2478@gmail.com>
2025-05-04 21:20:52 +1000

    Moved the move_and_slide call into process as that seems to have fixed to camera jitter, additionally the player has a limit on how high / low they can look. I do not understand the maths

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
-@export var mouseSensitivty: float = 0.01
+@export var mouseSensitivty: float = 0.005
 
 
 @onready var pivot: Node3D = $Pivot
@@ -20,12 +20,14 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
+			
 			pivot.rotate_y(-event.relative.x * mouseSensitivty)
 			camera_3d.rotate_x(-event.relative.y * mouseSensitivty)
+			camera_3d.rotation.x = clampf(camera_3d.rotation.x, -PI/2, PI/2)
 
 func _physics_process(delta):
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction = (pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
@@ -40,5 +42,6 @@ func _physics_process(delta):
 		if Input.is_action_pressed("jump"):
 			velocity.y = 30
 	
-	# Moving the Character
+
+func _process(delta: float) -> void:
 	move_and_slide()
