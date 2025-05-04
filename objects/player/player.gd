ce2fffc356e859569d40798f3f7197415288a2a9
CWmain <cameronwhite2478@gmail.com>
2025-05-04 19:51:21 +1000

    Camera now rotates with mouse, now need to limit the rotation

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -4,38 +4,41 @@ extends CharacterBody3D
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
+@export var mouseSensitivty: float = 0.01
+
+
+@onready var pivot: Node3D = $Pivot
+@onready var camera_3d: Camera3D = $Pivot/Camera3D
 
 var target_velocity = Vector3.ZERO
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			pivot.rotate_y(-event.relative.x * mouseSensitivty)
+			camera_3d.rotate_x(-event.relative.y * mouseSensitivty)
+
 func _physics_process(delta):
-	var direction = Vector3.ZERO
-
-	if Input.is_action_pressed("move_right"):
-		print("asdas")
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-
-	#if direction != Vector3.ZERO:
-		#direction = direction.normalized()
-		#$Pivot.basis = Basis.looking_at(direction)
-
-	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+		velocity.y = velocity.y - (fall_acceleration * delta)
 	else:
 		if Input.is_action_pressed("jump"):
-			print("jump")
-			target_velocity.y = 30
-
+			velocity.y = 30
+	
 	# Moving the Character
-	velocity = target_velocity
 	move_and_slide()
